14e9bb98b610b176d1b437b2016929d0110f4335
Khanh Dinh <khanh.dinh@netpower.no>
2023-06-12 08:11:26 +0700

    format code

--- fake_new_detection.py
+++ fake_new_detection.py
@@ -18,22 +18,27 @@ image = Image.open('./data/output.png')
 st.image(image)
 form = st.form(key='my_form')
 text_input = form.text_input('Insert a piece of news here')
-option = form.selectbox('Choose your model', ('Logistic Regression', 'Long short-term memory'))
+option = form.selectbox('Choose your model',
+                        ('Logistic Regression', 'Long short-term memory'))
 submit_button = form.form_submit_button('Predict')
 
 # function
+
+
 def create_model():
-    # Sequential Model  
+    # Sequential Model
     model = Sequential()
     # embeddidng layer
-    model.add(Embedding(8486, output_dim = 128))
+    model.add(Embedding(8486, output_dim=128))
     # Bi-Directional RNN and LSTM
     model.add(Bidirectional(LSTM(128)))
     # # Dense layers
-    model.add(Dense(128, activation = 'relu'))
-    model.add(Dense(1,activation= 'sigmoid'))
-    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc'])
+    model.add(Dense(128, activation='relu'))
+    model.add(Dense(1, activation='sigmoid'))
+    model.compile(optimizer='adam',
+                  loss='binary_crossentropy', metrics=['acc'])
     return model
+
 
 def pred(x):
     if x > 0.5:
@@ -41,35 +46,37 @@ def pred(x):
     else:
         return 0
 
+
 def main():
     X_df = pd.read_csv('./data/X_df.csv')
     class_name = ["Fake news", "Real news"]
 
     if option == 'Logistic Regression':
         # load model
-        pickle_in = open('./model/lr_model.pkl', 'rb') 
+        pickle_in = open('./model/lr_model.pkl', 'rb')
         lr = pickle.load(pickle_in)
-        tfidf = TfidfVectorizer(ngram_range= (1,3), lowercase= True, max_features= 5000)
+        tfidf = TfidfVectorizer(ngram_range=(
+            1, 3), lowercase=True, max_features=5000)
         X_transformed = tfidf.fit_transform(X_df['clean_joined'])
         text_transformed = tfidf.transform([text_input])
         result = lr.predict(text_transformed)
         class_id = np.argmax(result)
-        st.write('Result: ', class_name[class_id])    
+        st.write('Result: ', class_name[class_id])
 
     if option == 'Long short-term memory':
         # create model
         model = create_model()
         # load model
         model.load_weights('./model/lstm_model/')
-        tokenizer = Tokenizer(num_words = 8486)
+        tokenizer = Tokenizer(num_words=8486)
         tokenizer.fit_on_texts(X_df['clean_joined'])
         test = tokenizer.texts_to_sequences([text_input])
         result = pred(model.predict(test)[0][0])
-        st.write('Result: ', class_name[result])    
+        st.write('Result: ', class_name[result])
+
 
 if __name__ == "__main__":
     try:
-    	main()
+        main()
     except:
-    	pass
-    	
+        pass
